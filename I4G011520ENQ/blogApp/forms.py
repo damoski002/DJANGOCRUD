c0ee0545ef60ab86eedb4e7420559a48809f96ff
Damoski002 <damoski002@gmail.com>
2022-06-27 14:05:55 +0100

    changes

--- I4G011520ENQ/blogApp/forms.py
+++ I4G011520ENQ/blogApp/forms.py
@@ -2,10 +2,10 @@ from socket import fromshare
 from attr import fields
 from django import froms
 from matplotlib.pyplot import title
-from .models import blogApp
-class blogAppForm(forms):
+from .models import Post
+class PostForm(forms.Form):
     class meta:
-        model = blogApp 
+        model = Post
         fields = [
             "title",
             "description"
